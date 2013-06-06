57c23d2d0a8fff62ced7dc501e0b2289993e0c39
anjars <anjars@gmail.com>
2013-06-06 16:52:49 +0200

    La til tester for å forhindre feil brukernavn, doble brukere

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
@@ -9,6 +9,16 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
   				  :first_name, :last_name, :profile_name
   # attr_accessible :title, :body
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, 
+                           uniqueness: true,
+                           format: {
+                            with: /a-zA-Z0-9_-/,
+                            message: 'must be correctly formatted.'
+                           }
+
 
   has_many :statuses
 
